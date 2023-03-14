e4703be6227151f28fec8e1359be0311fb7eb72f
Manisha Mogare <125579696+Manishamogare@users.noreply.github.com>
2023-03-14 23:46:25 +0530

    Update conversion_page.py

--- conversion_page.py
+++ conversion_page.py
@@ -163,7 +163,7 @@ def conversion():
                 Ne = 5315/num1
                 Nm = 9000/num1
                  
-         if st.button("Calculate result"):
+        if st.button("Calculate result"):
 
             st.write("##### in English count (Ne) = ",Ne,"Ne")
             st.write("##### in Metric count (Nm) = ",Nm,"Nm")
