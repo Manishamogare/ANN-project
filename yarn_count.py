d0b5776c6c49cc065d5a19402b69caec11e2d1a3
Manisha Mogare <125579696+Manishamogare@users.noreply.github.com>
2023-04-09 13:44:32 +0530

    Update yarn_count.py

--- yarn_count.py
+++ yarn_count.py
@@ -5,49 +5,49 @@ from conversion_page import conversion
 
 def yarn_count():
     # input 1       
-        select1 = st.selectbox('Select length in',options=("km","m","cm","mm","yd","ft","inch"))
-        num1 = st.number_input(label="Enter length")
-        
-        
-        # input 2
-        select2 = st.selectbox('Select weight in',options=("kg","gm","mg","pounds"))
-        num2 = st.number_input(label="Enter weight")
+    select1 = st.selectbox('Select length in',options=("km","m","cm","mm","yd","ft","inch"))
+    num1 = st.number_input(label="Enter length")
 
-        st.write("#### Different Types Yarn Counts")
-        
-        if select1 == "km":
-            yd = num1 * 1093.61
-            m = num1 * 1000
-            cm = num1 * 100000
-            mm = num1 * 1000000
-            ft = num1 * 3280.8399
-            inch = num1 * 39370.0787
 
-        operation = st.radio("Select a type of yarn count:",
-                            ("Tex", "Den", "Ne", "Nm","Grains/yd"))
+    # input 2
+    select2 = st.selectbox('Select weight in',options=("kg","gm","mg","pounds"))
+    num2 = st.number_input(label="Enter weight")
 
-        ans = 0
+    st.write("#### Different Types Yarn Counts")
 
-        def calculate():
-            if operation == "Tex":
-                
-                    
-                
-                ans = num1 + num2
-            elif operation == "DEn":
-                ans = num1 - num2
-            elif operation == "Ne":
-                ans = num1 * num2
-            elif operation=="Divide" and num2!=0:
-                ans = num1 / num2
-            else:
-                st.warning("Division by 0 error. Please enter a non-zero number.")
-                ans = "Not defined"
+    if select1 == "km":
+        yd = num1 * 1093.61
+        m = num1 * 1000
+        cm = num1 * 100000
+        mm = num1 * 1000000
+        ft = num1 * 3280.8399
+        inch = num1 * 39370.0787
 
-            st.success(f"Answer = {ans}")
+    operation = st.radio("Select a type of yarn count:",
+                        ("Tex", "Den", "Ne", "Nm","Grains/yd"))
 
-        if st.button("Calculate result"):
-            calculate()
-            
-  
-        
+    ans = 0
+
+    def calculate():
+        if operation == "Tex":
+
+
+
+            ans = num1 + num2
+        elif operation == "DEn":
+            ans = num1 - num2
+        elif operation == "Ne":
+            ans = num1 * num2
+        elif operation=="Divide" and num2!=0:
+            ans = num1 / num2
+        else:
+            st.warning("Division by 0 error. Please enter a non-zero number.")
+            ans = "Not defined"
+
+        st.success(f"Answer = {ans}")
+
+    if st.button("Calculate result"):
+        calculate()
+
+
+
